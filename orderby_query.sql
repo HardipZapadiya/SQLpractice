e09893165ab05314d167d076d9fb19c115fd7aaf
HardipZapadiya <hardipzapadiya18@gmail.com>
2025-07-27 09:43:45 +0530

    Ascending Order

--- orderby_query.sql
+++ orderby_query.sql
@@ -4,4 +4,12 @@ use MyDatabase;
 --ordering in descending order 
 select *
 from customers 
-order by score desc
+order by score desc;
+
+--order in Ascending order 
+--by default record will be order in ascending order 
+
+
+select * 
+from customers 
+order by score asc;
